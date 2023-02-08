e0d7e827865d8498bb64f53632da832bd0c17b4b
santosomar <os@cisco.com>
2023-02-08 05:46:24 +0000

    updating setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Common Security Advisory Framework (CSAF) Validator',
     version='0.1',
+    author='Omar Santos',
     description='This program validates CSAF JSON documents against the CSAF 2.0 schema.',
     packages=find_packages(),
     install_requires=[
